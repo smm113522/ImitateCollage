321827d32d5a7f8ca5e499a841b8dc2b7f70d04d
客观开发者 <908085643@qq.com>
2017-12-21 09:04:37 +0800

    初始化

---
+++ ui/module-basic/src/main/java/com/xunmeng/pinduoduo/basic/BaseActvity.java
@@ -0,0 +1,12 @@
+package com.xunmeng.pinduoduo.basic;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/12/21 0021.
+ */
+
+public class BaseActvity extends AppCompatActivity {
+
+
+}
