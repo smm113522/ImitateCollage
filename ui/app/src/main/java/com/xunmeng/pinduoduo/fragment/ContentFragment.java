8285573c9e84762e04a12c012ab51fe1e10452cb
客观开发者 <908085643@qq.com>
2017-12-25 18:01:29 +0800

    1，首页添加

---
+++ ui/app/src/main/java/com/xunmeng/pinduoduo/fragment/ContentFragment.java
@@ -0,0 +1,20 @@
+package com.xunmeng.pinduoduo.fragment;
+
+import com.xunmeng.pinduoduo.R;
+import com.xunmeng.pinduoduo.basic.BaseFragment;
+
+/**
+ * Created by Administrator on 2017/12/25 0025.
+ */
+
+public class ContentFragment extends BaseFragment implements OnTabReselectListener {
+    @Override
+    public void onTabReselect() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_content;
+    }
+}
--- ui/app/src/main/java/com/xunmeng/pinduoduo/fragment/HomeFragment.java
+++ ui/app/src/main/java/com/xunmeng/pinduoduo/fragment/HomeFragment.java
@@ -1,14 +1,18 @@
 package com.xunmeng.pinduoduo.fragment;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 import com.xunmeng.pinduoduo.R;
 import com.xunmeng.pinduoduo.basic.BaseFragment;
+import com.xunmeng.pinduoduo.widget.smarttablayout.SmartTabLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -31,4 +35,28 @@ public class HomeFragment extends BaseFragment implements OnTabReselectListener 
         Toast.makeText(getContext(), "ddd", Toast.LENGTH_LONG).show();
     }
 
+    @Override
+    protected void initWidget(View root) {
+        super.initWidget(root);
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getChildFragmentManager(), FragmentPagerItems.with(getContext())
+                .add("11", ContentFragment.class)
+                .add("222", ContentFragment.class)
+                .create());
+
+        ViewPager viewPager = (ViewPager) root.findViewById(R.id.viewpager);
+        viewPager.setAdapter(adapter);
+
+        SmartTabLayout viewPagerTab = (SmartTabLayout) root.findViewById(R.id.viewpagertab);
+        viewPagerTab.setViewPager(viewPager);
+
+    }
+
+    @Override
+    protected void initData() {
+        super.initData();
+
+
+
+    }
 }
